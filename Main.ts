2eb94ae3a72ce8546ee1ea697f415d6202aca295
KY <nguyenhoangky23121997@gmail.com>
2023-04-10 15:36:28 +0700

    ..

--- Main.ts
+++ Main.ts
@@ -6,11 +6,11 @@ import {Circle} from "./Circle";
 let circle1 = new Circle(10)
 let rectangle1 =new Rectangle(5,10)
 let square1 = new Square(5)
-let Arr=[circle1,rectangle1,square1]
+let Arr:(Circle|Rectangle)[]=[circle1,rectangle1,square1]
 
 Arr.forEach((e)=>{
-    e.resize(20)
-    console.log(e.getArea())
+    e.resize(Math.random()*100)
+    console.log(`${e.getArea()}`)
 })
 console.table(Arr)
 
