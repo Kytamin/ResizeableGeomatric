d81ebe2eedb9d733ce8ab767fccbf0bdf4c04746
KY <nguyenhoangky23121997@gmail.com>
2023-04-10 14:49:26 +0700

    ..

--- Main.ts
+++ Main.ts
@@ -2,15 +2,18 @@ import {Rectangle} from "./Rectangle";
 import {Resizeable} from "./Resizeable";
 import {Square} from "./Square";
 import {Circle} from "./Circle";
+
 let circle1 = new Circle(10)
 let rectangle1 =new Rectangle(5,10)
 let square1 = new Square(5)
 let Arr=[circle1,rectangle1,square1]
 
 Arr.forEach((e)=>{
-    e.resize(50)
+    e.resize(20)
+    console.log(e.getArea())
 })
-console.log(Arr)
+console.table(Arr)
+
 
 
 
